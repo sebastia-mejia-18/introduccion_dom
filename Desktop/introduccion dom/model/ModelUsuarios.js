d50d3892c82c49e1d0999a7446b9be2c7d4423e1
omar gallego <ogallego@cesde.net>
2024-10-09 15:35:54 -0500

    se pausa el proyecto

---
+++ Desktop/introduccion dom/model/ModelUsuarios.js
@@ -0,0 +1,25 @@
+export let usuarios = [
+ {
+     usuario: "mini-admin",
+     contraseña: '1234567',
+     id:1
+ },
+ {
+    usuario: "super-admin",
+    contraseña: '654321',
+    id:2
+},
+{
+     usuario: "super-admin",
+     contraseña: '1234566',
+     id:3
+ },
+ {
+    usuario:'usuario-final',
+    contraseña:'123456',
+    id:4
+ }
+ 
+]
+
+/*objetos*/
